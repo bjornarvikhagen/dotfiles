fc8bec44bb5c2fce73edbf923b9ab17b9370fb41
Bjørnar Vikhagen <bjornar.vikhagen@dipai.no>
2025-02-13 08:03:00 +0100

    Enhance apply.sh

--- apply.sh
+++ apply.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
-# clone repo if it doesn't exist
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m' 
+
+echo -e "${BLUE}
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⣦⣤⣾⣿⠿⠛⣋⣥⣤⣀⠀⠀⠀⠀
+⠀⠀⠀⠀⡤⡀⢈⢻⣬⣿⠟⢁⣤⣶⣿⣿⡿⠿⠿⠛⠛⢀⣄⠀
+⠀⠀⢢⣘⣿⣿⣶⣿⣯⣤⣾⣿⣿⣿⠟⠁⠄⠀⣾⡇⣼⢻⣿⣾
+⣰⠞⠛⢉⣩⣿⣿⣿⣿⣿⣿⣿⣿⠋⣼⣧⣤⣴⠟⣠⣿⢰⣿⣿
+⣶⡾⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣈⣩⣤⡶⠟⢛⣩⣴⣿⣿⡟
+⣠⣄⠈⠀⣰⡦⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡛⠛⠛⠁
+⣉⠛⠛⠛⣁⡔⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠥⠀⠀
+⣭⣏⣭⣭⣥⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢠${NC}
+"
+
 if [ ! -d "$HOME/dotfiles" ]; then
-    echo "cloning dotfiles..."
+    echo -e "${BLUE}📦 cloning dotfiles...${NC}"
     git clone https://github.com/bjornarvikhagen/dotfiles.git "$HOME/dotfiles"
 fi
 
-# neofetch config
-echo "installing neofetch config..."
+echo -e "${BLUE}installing neofetch config...${NC}"
 mkdir -p ~/.config/neofetch
 cp "$HOME/dotfiles/neofetch/config.json" ~/.config/neofetch/config.conf
+echo -e "${BLUE}neofetch config installed${NC}"
 
-# cursor settings
-echo "installing cursor settings..."
-mkdir -p ~/.config/Cursor/User
-cp "$HOME/dotfiles/cursor/settings.json" ~/.config/Cursor/User/settings.json
+echo -e "${GREEN}Which editor are you using?${NC}"
+select editor in "Cursor" "VSCode" "Skip"; do
+    case $editor in
+        "Cursor")
+            echo -e "${BLUE}installing cursor settings...${NC}"
+            cp "$HOME/dotfiles/cursor/settings.json" "$HOME/Library/Application Support/Cursor/User/settings.json"
+            break
+            ;;
+        "VSCode")
+            echo -e "${BLUE}installing vscode settings...${NC}"
+            cp "$HOME/dotfiles/cursor/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+            break
+            ;;
+        "Skip ⏭ ")
+            echo -e "${RED}⏭  skipping editor settings...${NC}"
+            break
+            ;;
+    esac
+done
 
-echo "done! restart your apps to see changes"
+echo -e "${GREEN}✨ done! restart your apps to see changes${NC}"
